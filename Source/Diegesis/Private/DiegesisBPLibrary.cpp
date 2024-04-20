72614566ae86fb582accd618067fb13d8d43afe8
ChrisWhisker <cdworces@ncsu.edu>
2024-04-20 17:51:54 -0400

    Add initial plugin files

---
+++ Source/Diegesis/Private/DiegesisBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DiegesisBPLibrary.h"
+#include "Diegesis.h"
+
+UDiegesisBPLibrary::UDiegesisBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UDiegesisBPLibrary::DiegesisSampleFunction(float Param)
+{
+	return -1;
+}
+
